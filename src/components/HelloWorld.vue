bf621637b9694e6a3c940a0b250a2c225dd6e097
Aabhusan <maharjanaabhusan@gmail.com>
2019-09-28 11:29:30 +0545

    hello world

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>Helloworld</div>
+        <div>{{message}}</div> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelloWorld",
+    data () {
+    return {
+      message: "biskash dai is awesome programmer"
+    }
+  }
+};
+</script>
+<style>
+
+</style>
